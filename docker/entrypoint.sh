c4aea8e6e620bde37bad6ad47e1a67374eda37ee
cfridgen <cfridgen@users.noreply.github.com>
2026-05-07 18:26:25 +0200

    Add docker-compose.yml with DBus socket mount and Bluetooth caps

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -eu
 
+# DBus: default to system bus socket path; warn if not mounted
+DBUS_SYSTEM_BUS_ADDRESS="${DBUS_SYSTEM_BUS_ADDRESS:-unix:path=/run/dbus/system_bus_socket}"
+export DBUS_SYSTEM_BUS_ADDRESS
+_dbus_socket="${DBUS_SYSTEM_BUS_ADDRESS#unix:path=}"
+if [ ! -S "${_dbus_socket}" ]; then
+  echo "WARNING: DBus socket '${_dbus_socket}' not found – Bluetooth scan will not work." >&2
+fi
+
 NODE_EXPORTER_ADDR="${NODE_EXPORTER_ADDR:-:9100}"
 NODE_EXPORTER_ENABLED="${NODE_EXPORTER_ENABLED:-1}"
 NODE_EXPORTER_ARGS="${NODE_EXPORTER_ARGS:-}"
