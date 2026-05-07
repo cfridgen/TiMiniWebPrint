78d2749009a6374251a502bdfdaac2eb2dcb3877
cfridgen <cfridgen@users.noreply.github.com>
2026-05-07 18:03:11 +0200

    Make node_exporter configurable via NODE_EXPORTER_ENABLED and NODE_EXPORTER_ARGS

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,10 +2,16 @@
 set -eu
 
 NODE_EXPORTER_ADDR="${NODE_EXPORTER_ADDR:-:9100}"
+NODE_EXPORTER_ENABLED="${NODE_EXPORTER_ENABLED:-1}"
+NODE_EXPORTER_ARGS="${NODE_EXPORTER_ARGS:-}"
 APP_ADDR="${APP_ADDR:-0.0.0.0}"
 APP_PORT="${APP_PORT:-8000}"
 
-node_exporter --web.listen-address="${NODE_EXPORTER_ADDR}" &
+if [ "${NODE_EXPORTER_ENABLED}" = "1" ]; then
+  # Extra args can be passed via NODE_EXPORTER_ARGS to disable noisy collectors.
+  # shellcheck disable=SC2086
+  node_exporter --web.listen-address="${NODE_EXPORTER_ADDR}" ${NODE_EXPORTER_ARGS} &
+fi
 
 exec python -m uvicorn timiniprint.app.web:app \
   --host "${APP_ADDR}" \
